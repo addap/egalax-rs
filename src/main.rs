e1555a134be75f50b537131856feaaecc12bc410
Adrian Dapprich <adrian@dappri.ch>
2022-04-03 23:32:38 +0200

    add udev rules and systemd service to start driver when plugging in device.
    But this is more complicated than I'd like. I think the better option is to always have the driver running and use libudev to listen for new devices there

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,17 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let usage = "usage: sudo ./target/debug/egalax-rs /dev/hidraw0";
 
     let node_path = std::env::args().nth(1).expect(usage);
-    log::info!("Using raw device node {}", node_path);
+    log::info!("Using raw device node '{}'", node_path);
     let device_node = OpenOptions::new().read(true).open(&node_path).unwrap();
 
-    let monitor_name = std::env::args().nth(2);
-    if let Some(monitor_name) = &monitor_name {
-        log::info!("Using monitor {}", monitor_name);
-    }
-    let monitor_cfg = MonitorConfigBuilder::from_file("./config")?
-        .with_name(monitor_name)
-        .build()?;
+    // let monitor_name = std::env::args().nth(2);
+    // if let Some(monitor_name) = &monitor_name {
+    //     log::info!("Using monitor {}", monitor_name);
+    // }
+    let monitor_cfg =
+        MonitorConfigBuilder::from_file("/home/adrian/programming/rust/egalax-rs/config")?
+            // .with_name(monitor_name)
+            .build()?;
     log::info!("Using monitor config {}", monitor_cfg);
 
     virtual_mouse(device_node, monitor_cfg)?;
